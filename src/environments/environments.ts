c76d6a49242d814f81851aea82172951d5a1ee84
Ismael B B <ismbou9@alu.edu.gva.es>
2024-02-15 08:33:38 +0100

    j

---
+++ src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environments = {
+    production: false,
+    supabaseUrl: 'https://untktmroloftabtrymov.supabase.co',
+    supabaseKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVudGt0bXJvbG9mdGFidHJ5bW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNjY0MzYsImV4cCI6MjAxNDg0MjQzNn0.9UCatd_9uVYjwsztpV1xbsOaM3PEq3gq_kBxJPXxBsM',
+  };
+  
